e83c39237ad654d9b042721503a0cee34a98e7b6
Nandbert <nandbert@bayern-mail.de>
2016-03-19 16:53:12 -0700

    add a simple app

---
+++ apps/hello/BUILD.mk
@@ -0,0 +1,17 @@
+#
+#  CONTENT
+#    
+#
+#  AUTHOR
+#    Norbert
+#
+#  DATE
+#    2016-03-10
+#
+#  LANGUAGE
+#    make
+#
+
+GNU_APP =	hello
+GNU_APP_SRCS =	hello.c
+
---
+++ apps/hello/hello.c
@@ -0,0 +1,16 @@
+/** 
+ *
+ *
+ *  \file      hello.c
+ *  \author    Norbert
+ *  \date      2016-03-10
+ */
+#include	<stdio.h>
+
+
+int main(void)
+{
+    printf("hello world\n");
+
+    return 0;
+}
